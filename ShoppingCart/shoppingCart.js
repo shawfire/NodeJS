d4da5eab632c03749220714e43d57e9c53768267
John Shaw <john.shaw@rxpservices.com>
2017-06-15 10:43:48 +1000

    ShoppingCart refactor.

--- ShoppingCart/shoppingCart.js
+++ ShoppingCart/shoppingCart.js
@@ -1,9 +1,9 @@
-productCatalogue = [
-    { code: 'ult_small', name: 'Unlimited 1GB', price: 24.90 },
-    { code: 'ult_medium', name: 'Unlimited 2GB', price: 29.90 },
-    { code: 'ult_large', name: 'Unlimited 5GB', price: 44.90 },
-    { code: '1gb', name: '1 GB Data-pack', price: 9.90 }
-];
+productCatalogue = {
+    'ult_small': { code: 'ult_small', name: 'Unlimited 1GB', price: 24.90 },
+    'ult_medium': { code: 'ult_medium', name: 'Unlimited 2GB', price: 29.90 },
+    'ult_large': { code: 'ult_large', name: 'Unlimited 5GB', price: 44.90 },
+    '1gb': { code: '1gb', name: '1 GB Data-pack', price: 9.90 }
+};
 
 class ShoppingCart1 {
     constructor(pricingRules) {
